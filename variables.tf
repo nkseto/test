d61622114ab4d6ea4064dbbb03ccc6c4f8e8a875
nkseto <n.k.seto@gmail.com>
2020-06-25 14:40:31 -0400

    generate TF

---
+++ variables.tf
@@ -0,0 +1,15 @@
+variable "ibm_container_cluster___cluster_instance___datacenter" {
+  description = "IBM Kube cluster datacenter"
+}
+variable "ibm_container_cluster___cluster_instance___name" {
+  description = "IBM Kube cluster name"
+}
+variable "ibmcloud_resource_group" {
+  description = "IBM Cloud Resource Group"
+}
+variable "ibmcloud_api_key" {
+}
+variable "TF_VERSION" {
+  description = "terraform engine version to be used in schematics"
+  default     = "0.12"
+}
